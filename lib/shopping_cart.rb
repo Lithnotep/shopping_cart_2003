ef89f8277f72ea113b8df4708501620396308263
Max Mitrani <mjmitrani168@gmail.com>
2020-03-25 10:10:24 -0600

    add total_number_of_products method and test

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -7,9 +7,18 @@ attr_reader :name, :capacity, :products
     @name = name
     @capacity = capacity
     @products = []
+    @is_full = false
   end
 
   def add_product(product)
     @products << product
+  end
+
+  def total_number_of_products
+    product_total = 0
+    @products.each do |product|
+      product_total += product.quantity.to_i
+    end
+    return product_total
   end
 end
